88d0756141ed1f5b8690f57a5495cf75b4b5c228
vitalik1970 <ytcenko1970@gmail.com>
2024-02-16 10:37:19 +0200

    fix

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 const SlideShow = () => {
   const allImages = [
-    "./img/slav.png",
+    "./img/slav.jpg",
     "/img/slav1.png",
     "/img/slav2.png",
-    "/img/slav3.png",
+    "/img/slav3.jpg",
     "/img/slav4.png",
     "/img/slav5.jpg",
     "/img/slav6.jpg",
