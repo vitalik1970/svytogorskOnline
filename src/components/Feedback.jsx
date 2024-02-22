59da1cd072b53b294a3e23db1b86a707adb27a46
vitalik1970 <ytcenko1970@gmail.com>
2024-02-22 16:39:10 +0200

    fix

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -87,7 +87,11 @@ function Feedback(props) {
     </div>
     </div>
     <div className='map__google'>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10461.626843994178!2d37.56981345!3d49.04089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40df7fc7cf0c6fd1%3A0xa026fcdbeed1248e!2sSvyatohirs&#39;k%2C%20Donetsk%20Oblast%2C%20Ukraine%2C%2084130!5e0!3m2!1sen!2sus!4v1708180554765!5m2!1sen!2sus" width="1000" height="350" style={{border:"0"}}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10461.626843994178!2d37.56981345!3d49.04089!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40df7fc7cf0c6fd1%3A0xa026fcdbeed1248e!2sSvyatohirs&#39;k%2C%20Donetsk%20Oblast%2C%20Ukraine%2C%2084130!5e0!3m2!1sen!2sus!4v1708180554765!5m2!1sen!2sus" style={{
+                width: '100%',
+                height: '350px',
+                border: '0'
+            }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
     </div>
     : ''}</>
     
